e7369ed252039a8c0e1dd69f00ddb7210a6a6c1a
Muhammad Saim <skystarkrk@gmail.com>
2026-07-12 05:08:30 +0500

    Initial project setup with React Router architecture

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+// Router
+import { Routes, Route } from 'react-router-dom'
+// Layout
+import MainLayout from './layouts/MainLayout.jsx'
+// Pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Portfolio from "./pages/Portfolio";
+import Team from "./pages/Team";
+import Blog from "./pages/Blog";
+import Contact from "./pages/Contact";
+import NotFound from "./pages/NotFound";
+// CSS
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Routes>
+        <Route element={<MainLayout/>}>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/about' element={<About />}></Route>
+          <Route path='/blog' element={<Blog />}></Route>
+          <Route path='/contact' element={<Contact />}></Route>
+          <Route path='/portfolio' element={<Portfolio />}></Route>
+          <Route path='/services' element={<Services />}></Route>
+          <Route path='/team' element={<Team/>}></Route>
+        </Route>
+        <Route path='*' element={<NotFound />}></Route>
+      </Routes>
+    </>
+  )
+}
+
+export default App
