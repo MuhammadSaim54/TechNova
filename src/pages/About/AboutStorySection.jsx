38c8d74caad060cc363867585b2cd2f8080ca1c1
Muhammad Saim <skystarkrk@gmail.com>
2026-07-13 12:12:52 +0500

    feat(about): complete About page (Milestone 2 checkpoint)

--- src/pages/About/AboutStorySection.jsx
+++ src/pages/About/AboutStorySection.jsx
@@ -1,6 +1,34 @@
+import Data from './StorySectionData.js';
+import './AboutStorySection.css';
+
 function AboutStorySection() {
-    return(
+    return (
         <>
+            <section className="AboutStorySection">
+                <div className="About-Story-Detail">
+                    <div className="About-Story-Heading">
+                        <h2>Our Story</h2>
+                    </div>
+                    <div className="About-Story-Description">
+                        <p>TechNova helps businesses grow through modern digital solutions. Our focus is on creating high-quality products, building strong client relationships, and delivering results that make a lasting impact.</p>
+                    </div>
+                </div>
+                <div className="About-Story-Cards">
+                    {Data.map((card) => {
+                        return (
+                            <div className={card.className} key={card.id}>
+                                <div className="icon">
+                                    <card.icon size={38} color='#0051fb'/>
+                                </div>
+                                <div className="details">
+                                    <span>{card.title}</span>
+                                    <p>{card.description}</p>
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
+            </section>
         </>
     )
 }
---
+++ src/pages/About/StorySectionData.js
@@ -0,0 +1,27 @@
+import { Trophy, ClipboardCheck, BriefcaseBusiness } from 'lucide-react';
+
+let data = [
+    {
+        id: "1",
+        title: "10+",
+        description: "Years of Experience",
+        className: "card Years-Card",
+        icon: Trophy
+    },
+    {
+        id: "2",
+        title: "120+",
+        description: "Projects Completed",
+        className: "card Project-Card",
+        icon: BriefcaseBusiness
+    },
+    {
+        id: "3",
+        title: "98%",
+        description: "Client Satisfaction",
+        className: "card Client-Card",
+        icon: ClipboardCheck
+    }
+]
+
+export default data
