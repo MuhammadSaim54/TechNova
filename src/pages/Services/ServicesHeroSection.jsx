55236e95bac398ab806a2e894af704b6d8574c74
Muhammad Saim <skystarkrk@gmail.com>
2026-08-12 11:41:20 +0500

    refactor(services): migrate hero section to Tailwind CSS

--- src/pages/Services/ServicesHeroSection.jsx
+++ src/pages/Services/ServicesHeroSection.jsx
@@ -1,21 +1,76 @@
-import './ServicesHeroSection.css';
-
 function ServicesHeroSection() {
-    return(
-        <>
-        <section className="ServicesHeroSection">
-            <div className="Services-Badge">
-                <span>OUR SERVICES</span>
+    return (
+        <section
+            className="
+                flex
+                flex-col
+                justify-center
+                items-center
+
+                p-6
+                gap-3
+
+                max-[768px]:items-start
+            "
+        >
+
+            {/* ================= Badge ================= */}
+
+            <div>
+                <span
+                    className="
+                        font-bold
+                        font-['Manrope']
+                        text-(--Primary)
+
+                        max-[768px]:text-[0.85rem]
+                    "
+                >
+                    OUR SERVICES
+                </span>
             </div>
-            <div className="Services-Heading">
-                <h2>Services That Drive Results</h2>
+
+
+            {/* ================= Heading ================= */}
+
+            <div>
+                <h2
+                    className="
+                        text-[2.75rem]
+                        font-semibold
+
+                        max-[768px]:text-[2.25rem]
+                    "
+                >
+                    Services That Drive Results
+                </h2>
             </div>
-            <div className="Services-Description">
-                <p>We provide a wide range of digital services to help your buisness to grow faster and reach the right audience.</p>
+
+
+            {/* ================= Description ================= */}
+
+            <div>
+                <p
+                    className="
+                        w-full
+                        font-['Manrope']
+                        text-(--Text-Muted)
+
+                        max-w-[30rem]
+
+                        text-center
+
+                        max-[768px]:text-[0.85rem]
+                        max-[768px]:text-left
+                    "
+                >
+                    We provide a wide range of digital services to help your
+                    buisness to grow faster and reach the right audience.
+                </p>
             </div>
+
         </section>
-        </>
-    )
+    );
 }
 
-export default ServicesHeroSection
+export default ServicesHeroSection;
